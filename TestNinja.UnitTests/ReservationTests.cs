f43b706f06bd6c2801fb95f944c4fa382b6d8237
Ubaid Rana <ubaidrana45@gmail.com>
2020-05-17 15:48:06 +0200

    Wrote 1st Unit Test

---
+++ TestNinja.UnitTests/ReservationTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestClass]
+    public class ReservationTests
+    {
+        [TestMethod]
+        public void CanBeCanelledBy_UserIsAdmin_ReturnsTrue()
+        {
+            // 1 - Arrange
+
+            var reservation = new Reservation();
+
+            // 2 - Act
+
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = true });
+
+            // 3 - Assert
+
+            Assert.IsTrue(result);
+        }
+    }
+}
