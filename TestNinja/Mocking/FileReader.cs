744553600c65fee630ef6242fb198c5ce3351b74
Ubaid Rana <ubaidrana45@gmail.com>
2020-05-18 01:31:46 +0200

    Solved Stack Exercise

---
+++ TestNinja/Mocking/FileReader.cs
@@ -0,0 +1,13 @@
+using System.IO;
+
+namespace TestNinja.Mocking
+{
+
+    public class FileReader : IFileReader
+    {
+        public string Read(string path)
+        {
+            return File.ReadAllText(path);
+        }
+    }
+}
