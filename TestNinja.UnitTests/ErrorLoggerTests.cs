9f3a3f37140af959750dd6e85563d5d150041102
Ubaid Rana <ubaidrana45@gmail.com>
2020-05-17 23:09:51 +0200

    Tested the methods that throws exceptions

--- TestNinja.UnitTests/ErrorLoggerTests.cs
+++ TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -6,12 +6,26 @@ namespace TestNinja.UnitTests
     [TestFixture]
     public class ErrorLoggerTests
     {
+        private ErrorLogger _errorLogger;
+        [SetUp]
+        public void SetUp()
+        {
+            _errorLogger = new ErrorLogger();
+        }
         [Test]
         public void Log_WhenCalled_SetTheLastErrorProperty()
         {
-            var logger = new ErrorLogger();
-            logger.Log("a");
-            Assert.That(logger.LastError, Is.EqualTo("a"));
+            _errorLogger.Log("a");
+            Assert.That(_errorLogger.LastError, Is.EqualTo("a"));
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void Log_InvalidError_ThrowArgumentNullException(string error)
+        {
+            Assert.That(() => _errorLogger.Log(error), Throws.ArgumentNullException);
         }
     }
 }
