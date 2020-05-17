f51eaafc46f39ae4f3134f0632492fcbf45357f8
Ubaid Rana <ubaidrana45@gmail.com>
2020-05-17 19:14:27 +0200

    Used Setup method to initialize common functionalities

--- TestNinja.UnitTests/MathTests.cs
+++ TestNinja.UnitTests/MathTests.cs
@@ -6,33 +6,35 @@ namespace TestNinja.UnitTests
     [TestFixture]
     public class MathTests
     {
+        private Math _math;
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new Math();
+        }
         [Test]
         public void Add_WhenCalled_ReturnsTheSumOfArguments()
         {
-            var math = new Math();
-            var result = math.Add(1, 2);
+            var result = _math.Add(1, 2);
             Assert.That(result, Is.EqualTo(3));
         }
 
         [Test]
         public void Max_FirstArgumentIsGreater_ReturnTheFirstArgument()
         {
-            var math = new Math();
-            var result = math.Max(2, 1);
+            var result = _math.Max(2, 1);
             Assert.That(result, Is.EqualTo(2));
         }
         [Test]
         public void Max_FirstArgumentIsGreater_ReturnTheSecondArgument()
         {
-            var math = new Math();
-            var result = math.Max(1, 2);
+            var result = _math.Max(1, 2);
             Assert.That(result, Is.EqualTo(2));
         }
         [Test]
         public void Max_ArgumentsAreEqual_ReturnTheSameArguments()
         {
-            var math = new Math();
-            var result = math.Max(1, 1);
+            var result = _math.Max(1, 1);
             Assert.That(result, Is.EqualTo(1));
         }
     }
