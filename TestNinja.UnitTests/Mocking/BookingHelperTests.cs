048477986af646a1c708f4f79795e3ee071e67b4
Ubaid Rana <ubaidrana45@gmail.com>
2020-05-18 19:33:25 +0200

    Implemented the next test BookingStartsBeforeAndFinishesInTheMiddleOfAnExistingBooking_ReturnExistingBooking

--- TestNinja.UnitTests/Mocking/BookingHelperTests.cs
+++ TestNinja.UnitTests/Mocking/BookingHelperTests.cs
@@ -30,6 +30,21 @@ namespace TestNinja.UnitTests.Mocking
                 _existingBooking
             }.AsQueryable());
         }
+        
+        [Test]
+        public void BookingStartsBeforeAndFinishesInTheMiddleOfAnExistingBooking_ReturnExistingBooking()
+        {
+            var result = BookingHelper.OverlappingBookingsExist(
+                new Booking
+                {
+                    Id = 1,
+                    ArrivalDate = Before(_existingBooking.ArrivalDate),
+                    DepartureDate = After(_existingBooking.ArrivalDate)
+                }, _repository.Object);
+            
+            Assert.That(result, Is.EqualTo(_existingBooking.Reference));
+        }
+
         [Test]
         public void BookingStartsAndFinishesBeforeAnExistingBooking_ReturnEmptyString()
         {
